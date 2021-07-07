10d609f0025f5f572888c4b7877b6c72d5407c69
IguanaAzul <ramon.griffo98@gmail.com>
2021-07-07 20:33:27 -0300

    Megaexercício 3

--- script/controles-marcacao.js
+++ script/controles-marcacao.js
@@ -12,17 +12,7 @@ let formato_controle_marcacao = document.querySelectorAll('input[name="formato-d
 let checkbox_retangular_controle_marcacao = formato_controle_marcacao[0]
 let checkbox_oval_controle_marcacao = formato_controle_marcacao[1]
 
-function controleMarcacao(marcacao) {
-    x_controle_marcacao.value = parseInt(marcacao.style.left)
-    y_controle_marcacao.value = parseInt(marcacao.style.top)
-    largura_controle_marcacao.value = parseInt(marcacao.style.width)
-    altura_controle_marcacao.value = parseInt(marcacao.style.height)
-    titulo_controle_marcacao.value = marcacao.getAttribute('data-titulo')
-    conteudo_controle_marcacao.value = marcacao.getAttribute('data-conteudo')
-    cor_controle_marcacao.value = marcacao.getAttribute('data-cor')
-    checkbox_retangular_controle_marcacao.checked = marcacao.getAttribute('data-formato') === 'formato-retangular'
-    checkbox_oval_controle_marcacao.checked = marcacao.getAttribute('data-formato') === 'formato-oval'
-}
+let controles = document.querySelector('.controles')
 
 checkbox_visibilidade.addEventListener('change', function() {
     for (let m of marcacoes) {
@@ -34,10 +24,43 @@ function clicaMarcacao(e) {
     document.querySelector(".selecionada").classList.remove("selecionada")
     let m = e.currentTarget
     m.classList.toggle("selecionada")
-    controleMarcacao(m)
+    x_controle_marcacao.value = parseInt(m.style.left)
+    y_controle_marcacao.value = parseInt(m.style.top)
+    largura_controle_marcacao.value = parseInt(m.style.width)
+    altura_controle_marcacao.value = parseInt(m.style.height)
+    titulo_controle_marcacao.value = m.getAttribute('data-titulo')
+    conteudo_controle_marcacao.value = m.getAttribute('data-conteudo')
+    cor_controle_marcacao.value = m.getAttribute('data-cor')
+    checkbox_retangular_controle_marcacao.checked = m.getAttribute('data-formato') === 'formato-retangular'
+    checkbox_oval_controle_marcacao.checked = m.getAttribute('data-formato') === 'formato-oval'
 
 }
 
 for (let m of marcacoes) {
     m.addEventListener('click', clicaMarcacao)
+}
+
+function controleMarcacao(e){
+    let selecionada = document.querySelector(".selecionada")
+    selecionada.style.left = `${x_controle_marcacao.value}px`
+    selecionada.style.top = `${y_controle_marcacao.value}px`
+    selecionada.style.width = `${largura_controle_marcacao.value}px`
+    selecionada.style.height = `${altura_controle_marcacao.value}px`
+    selecionada.setAttribute('data-titulo', titulo_controle_marcacao.value)
+    selecionada.setAttribute('data-conteudo', conteudo_controle_marcacao.value)
+    selecionada.setAttribute('data-cor', cor_controle_marcacao.value)
+}
+
+controles.addEventListener('change', controleMarcacao)
+for (let f of formato_controle_marcacao) {
+    f.addEventListener('change', function() {
+            let selecionada = document.querySelector(".selecionada")
+            if (selecionada.getAttribute('data-formato') === 'formato-retangular') {
+                selecionada.setAttribute('data-formato', 'formato-oval')
+            }
+            else{
+                selecionada.setAttribute('data-formato', 'formato-retangular')
+            }
+        }
+    )
 }
